2d02680339a457fcd81c8387776982f3ef109772
bunny112 <rakeshreddy92954@gmail.com>
2026-08-28 19:57:59 +0530

    Create admin account and seed products

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,17 +2,25 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password' => 'password',
-        ]);
+        User::updateOrCreate(
+            ['email' => 'admin@nova.test'],
+            [
+                'name' => 'NOVA Admin',
+                'password' => Hash::make('password'),
+                'is_admin' => true,
+            ]
+        );
+
+        $this->call(ProductSeeder::class);
     }
 }
